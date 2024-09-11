d47825e6373f19c992f6faff9881c9ffe4f92db7
Lauranne Sarribouette <lauranne.sarribouette@gmail.com>
2024-09-11 12:08:40 +0200

    add reminder and locations in program page

--- app/src/routes/program.jsx
+++ app/src/routes/program.jsx
@@ -1,3 +1,29 @@
 export default function Programme() {
-    return <div>Programme et lieux</div>
+    return (
+        <div id="program">
+            <div id="reminder">
+                <p id="reminder__weekend">UN WEEK-END DE MARIAGE !</p>
+                <p id="reminder__dates">Les 21 et 22 juin 2025</p>
+            </div>
+            <h1>Les Lieux</h1>
+            <div id="locations">
+                <h2>La mairie</h2>
+                <div className="location">
+                    <a href="https://maps.app.goo.gl/umuCCswchy2bbQ579" target="_blank" rel="noopener noreferrer">
+                        <p>Mairie de Martillac</p>
+                        <p>14 avenue Charles de Gaule</p>
+                        <p>33650 Martillac</p>
+                    </a>
+                </div>
+                <h2>Le domaine</h2>
+                <div className="location">
+                    <a href="https://maps.app.goo.gl/oTAzoJqzBzerqQGb6" target="_blank" rel="noopener noreferrer">
+                        <p>Le  Domaine de Larchey</p>
+                        <p>28 route de Larchey</p>
+                        <p>33650 Saint-Médard-d'Eyrans</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
 }
--- app/src/routes/todo.jsx
+++ app/src/routes/todo.jsx
@@ -12,6 +12,10 @@ export default function Todo() {
                 <li>Plus de retour à la ligne pour plus de lisibilité et légèreté ?</li>
                 <li>Styliser la partie avant les inspirations de tenue ?</li>
                 <li>En version ordinateur, quelle taille veut-on pour les titres imagés ? Même largeur que le texte (actuellement) ou bien plus petit ?</li>
+                <li>
+                    Le vert foncé est intense sur l'ordinateur avec la navbar, est-ce qu'on passe au vert amande, plus doux ? (couleur du bouton dans la navbar)<br />
+                    Ou alors on change la couleur de la navbar (fond vert foncé, texte blanc) juste pour cette page ?
+                </li>
                 <li>Formulaire : checkbox "j'ai bien pris connaissance du code couleur exigé (enculé) et je m'engage à le respecter", mail de l'humain</li>
             </ul>
         </div>
